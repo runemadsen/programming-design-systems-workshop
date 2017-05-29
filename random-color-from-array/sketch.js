a9c6d78f8c20c2acd6f47e7cdccd0bc6d35838f9
Rune Madsen <rune@runemadsen.com>
2017-05-29 10:56:15 -0500

    random color from array

--- random-color-from-array/sketch.js
+++ random-color-from-array/sketch.js
@@ -1,11 +1,28 @@
 function randomColor() {
-  return color(random(0, 40), random(40, 100), random(10, 60));
+
+  var colorlist = [
+   color(255, 0, 0),
+   color(0, 255, 0),
+   color(0, 0, 255),
+   color(100, 0, 255),
+   color(0, 100, 255),
+   color(255, 100, 0),
+   color(255, 0, 100),
+   color(100, 255, 0),
+   color(0, 255, 100)
+  ];
+
+  var index = floor(random(colorlist.length));
+  return colorlist[index];
+
+  // If you want to only use a color once. Keep in mind
+  // that at some point you'll run out of colors.
+  // return colorlist.splice(index, 1);
 }
 
 function setup() {
 
   createCanvas(500, 500);
-  colorMode(HSL);
   noStroke();
 
   background(randomColor());
